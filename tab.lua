dc4d38466e1cbc3ef0fcae4771c8d90f7a3a88c5
br <brian@recchia.name>
2020-07-14 02:16:03 -0400

    added comments

--- tab.lua
+++ tab.lua
@@ -2,7 +2,7 @@ CANCEL_DELAY_SECONDS = 0.15
 tabKeyTimer = hs.timer.delayed.new(CANCEL_DELAY_SECONDS, function ()
   sendTab = false
 end)
-delayedEnable = hs.timer.delayed.new(0, function () tabHotKey:enable() end)
+delayedEnable = hs.timer.delayed.new(0, function () tabHotKey:enable() end) -- there is a nominal delay after doing hs.hotkey:enable(), but setting it to 0 seems to suffice
 function tabDown (event)
     hs.eventtap.event.newKeyEvent(hs.keycodes.map.fn, true):post()
     sendTab = true
@@ -11,8 +11,8 @@ end
 function tabUp (event)
   hs.eventtap.event.newKeyEvent(hs.keycodes.map.fn, false):post()
     if sendTab then
-      tabHotKey:disable()
-      hs.timer.doAfter(0, function ()
+      tabHotKey:disable() -- temporarily disable so we can send an actual tab
+      hs.timer.doAfter(0, function () -- there is a nominal delay after doing hs.hotkey:disable(), but setting it to 0 seems to suffice
 	      hs.eventtap.keyStroke({}, 'tab')
 	      delayedEnable:start()
       end)
